bf3cb0efc43d510b18d6feedbddd6ee8e9486cf9
fritzmg <fmg@inspiredminds.at>
2021-08-09 19:15:34 +0100

    add some translations

--- contao/languages/en/tl_address_group.php
+++ contao/languages/en/tl_address_group.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
  */
 
 $GLOBALS['TL_LANG']['tl_address_group']['new'] = ['Create new address group', 'Creates a new address group.'];
+$GLOBALS['TL_LANG']['tl_address_group']['edit'] = ['Edit address group ID %s', 'Edit address group ID %s'];
 $GLOBALS['TL_LANG']['tl_address_group']['address_group_legend'] = 'Address group';
 $GLOBALS['TL_LANG']['tl_address_group']['address_group_content_legend'] = 'Verified address content';
 $GLOBALS['TL_LANG']['tl_address_group']['name'] = ['Name', 'Name of the address group.'];
